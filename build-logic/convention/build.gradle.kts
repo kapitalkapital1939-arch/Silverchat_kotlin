570aabd5c584ae918a289cedf433f25160f97836
kapitalkapital1939-arch <kapitalkapital1939@gmail.com>
2026-09-11 11:17:52 +0300

    Update build.gradle.kts

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -19,7 +19,7 @@ kotlin {
 
 dependencies {
     compileOnly("com.android.tools.build:gradle:8.5.0") // или твоя версия AGP из каталога
-    compileOnly("com.google.devtools.ksp:symbol-processing-gradle-plugin:2.3.0-1.0.24") // или версия KSP
+    compileOnly(libs.ksp.gradle.plugin) // или версия KSP
     compileOnly(libs.kotlin.gradle.plugin)
     compileOnly(libs.kotlin.compose.plugin)
     compileOnly(libs.hilt.gradle.plugin)
